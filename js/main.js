3560011353aa78637be66c2d97df9915d5f694bb
Samson <contact@samsonloftin.com>
2021-03-24 08:00:48 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@ initMap = () => {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.mapbox-streets'
+    id: 'mapbox/streets-v11'
   }).addTo(newMap);
 
   updateRestaurants();
